b1e082bec296634e8716f17ab8a046efad56c331
mukherjeeSumit <sumitmukherjee15@gmail.com>
2021-01-24 00:56:51 +0530

    Created an array of posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'card-posts';
+
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountain',
+      content: 'Here is a piture of a snowy mountain '
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'bike',
+      content: 'I did some biking today'
+    }
+  ];
 }
